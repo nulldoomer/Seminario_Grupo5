2187f863fa14d9533a3d9232e5c0eb2ab71e6e35
HuesosAce <pauloarielyepez@gmail.com>
2025-10-23 07:33:07 -0500

    feat: add sklearn library and pipeline implementation

--- scripts/data_processing.py
+++ scripts/data_processing.py
@@ -1,15 +1,16 @@
 import pandas as pd
+from sklearn.base import BaseEstimator, TransformerMixin
 
 class CreateDataframes:
 
-    def create(self, dataset):
+    def create(self, dataset_path):
 
         # Create the dataframes for the data we previously analysed
 
         dataframe_dict = {}
         sheet_names = [
-            "BALANCE", "BALANCE%",
-            "COMPOS CART","COMPOS CART%",
+            "BALANCE", "BALANCE %",
+            "COMPOS CART","COMPOS CART %",
             "INDICADORES"
         ]
 
@@ -18,17 +19,16 @@ class CreateDataframes:
         # and using only the columns of the table
 
         for sheet in sheet_names:
-            temp_dataframe = pd.read_excel(dataset, sheet_name= sheet,
-                                           skiprows=7,
-                                           usecols= "B:AI")
+            temp_dataframe = pd.read_excel(dataset_path, sheet_name= sheet,
+                                           skiprows=7)
             dataframe_dict[sheet] = temp_dataframe
 
         # Create each one of the dataframes from the dictionary
 
         dataframe_balance = dataframe_dict["BALANCE"]
-        dataframe_balance_ptj = dataframe_dict["BALANCE%"]
+        dataframe_balance_ptj = dataframe_dict["BALANCE %"]
         dataframe_composcart = dataframe_dict["COMPOS CART"]
-        dataframe_composcart_ptj = dataframe_dict["COMPOS CART%"]
+        dataframe_composcart_ptj = dataframe_dict["COMPOS CART %"]
         dataframe_indicadores = dataframe_dict["INDICADORES"]
 
         # Return all the dataframes created
@@ -36,10 +36,16 @@ class CreateDataframes:
         return (dataframe_balance, dataframe_balance_ptj, dataframe_composcart,
                 dataframe_composcart_ptj, dataframe_indicadores)
 
-class DataCleaning:
 
-    def clean(self, dataframe: pd.DataFrame):
 
+class DataCleaning(BaseEstimator, TransformerMixin):
+
+    def fit(self, X: pd.DataFrame, y= None):
+
+        return self
+
+
+    def transform(self, X: pd.DataFrame):
         # Drop the blank column which is in all the dataframes
         # axis specifies that is a column and inplace mute the same object
         # also use errors=ignore to avoid exceptions if the column doesn't 
@@ -48,10 +54,11 @@ class DataCleaning:
         # But I know that ain't gonna happen because i saw all the excel
         # and its all the same in the tables we use
 
-        dataframe.drop("BANCOS PRIVADOS VIVIENDA", axis=1, inplace=True,
+        X = X.copy()
+
+        X.drop("BANCOS PRIVADOS VIVIENDA", axis=1, inplace=True,
                        errors="ignore")
 
-        return dataframe
-
+        return X 
 
 
